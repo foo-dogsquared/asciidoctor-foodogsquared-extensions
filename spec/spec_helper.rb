ad2834cafeb3e29c5a30d2606fcd23d35f2d7d5a
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-29 09:56:43 +0800

    Setup RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'asciidoctor'
+require 'asciidoctor/foodogsquared-extensions'
+
+RSpec.configure do
+  def fixtures_dir
+    File.join __dir__, 'fixtures'
+  end
+
+  def fixtures_file path
+    File.join fixtures_dir, path
+  end
+end
